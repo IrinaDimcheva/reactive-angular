b9513fbc5840a0935aad85ddf7c366b4636fad4c
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-22 20:32:16 +0200

    Data modification in reactive style (stateless application)

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Course } from '../model/course';
 import { map, shareReplay } from 'rxjs/operators';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +15,11 @@ export class CoursesService {
       map((res) => res['payload']),
       shareReplay()
     );
+  }
+
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
   }
 }
