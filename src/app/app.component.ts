f0f1b70f59d1116d1d557ef6efcae5aa68da16f0
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-24 01:05:55 +0200

    Add login and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { LoadingService } from './loading/loading.service';
-import { MessagesService } from './messages/messages.service';
+import { Component } from '@angular/core';
+import { AuthStore } from './services/auth.store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent {
+  constructor(public auth: AuthStore) {}
 
-  ngOnInit() {}
-
-  logout() {}
+  logout() {
+    this.auth.logout();
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthStore } from '../services/auth.store';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent {
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router) {
-
-    this.form = fb.group({
+    private router: Router,
+    private auth: AuthStore
+  ) {
+    this.form = this.fb.group({
       email: ['test@angular-university.io', [Validators.required]],
-      password: ['test', [Validators.required]]
+      password: ['test', [Validators.required]],
     });
-
-  }
-
-  ngOnInit() {
-
   }
 
   login() {
-
     const val = this.form.value;
-
-
-
+    this.auth.login(val.email, val.password).subscribe(
+      () => this.router.navigateByUrl('/courses'),
+      (err) => alert('Login failed!')
+    );
   }
-
 }
